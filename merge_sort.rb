41b745727970da3f059dfd18518e287ec491a1a5
Nathan Lucy <cosmocoder1@gmail.com>
2021-02-03 11:15:49 -0600

    final commit

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,6 +1,3 @@
-arr = [6, 3, 1, 8, 2, 5, 4, 7]
-
-
 def divide_array (arr)
   left_arr = []
   right_arr = []
@@ -8,29 +5,23 @@ def divide_array (arr)
   return [left_arr, right_arr]  
 end  
 
-
 def merge (arr1, arr2)
   result = []
-  while !arr1.empty? > 0 && !arr2.empty? > 0 do
+  while !arr1.empty? && !arr2.empty? do
     arr1[0] < arr2[0] ? result.push(arr1.shift) : result.push(arr2.shift) 
   end  
    result + arr1 + arr2
 end  
 
-
 def sort (arr)
-
   if arr.length == 1 
     return arr
   end 
-
   divided_array = divide_array(arr)
-
-  left_arr = sort(divided_array[0].slice(0, arr.length - 1))
-  right_arr = sort(divided_array[1].slice(0, arr.length - 1))
-  merge(left_arr, right_arr)
-  
+  left_channel = sort(divided_array[0].slice(0, arr.length - 1))
+  right_channel = sort(divided_array[1].slice(0, arr.length - 1))
+  merge(left_channel, right_channel)
 end 
 
-
-sort(arr)
+arr = [6, 3, 1, 8, 2, 5, 4, 7]
+puts sort(arr)
