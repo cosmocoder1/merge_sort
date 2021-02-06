0ed86d1898d36bab1b4c2d8d5a064fa03149cee7
Nathan Lucy <cosmocoder1@gmail.com>
2021-02-06 10:04:28 -0600

    code refactored

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,27 +1,30 @@
-def divide_array (arr)
-  left_arr = []
-  right_arr = []
-  arr.map.with_index { |value, i| i <= (arr.length - 1) / 2 ? left_arr.push(value) : right_arr.push(value) }
-  return [left_arr, right_arr]  
+def split (arr)
+  left_array = []
+  right_array = []
+  arr.map.with_index { |value, i| i <= (arr.length - 1) / 2 ? 
+  left_array.push(value) : right_array.push(value) }
+  return [left_array, right_array]
 end  
+
 
 def merge (arr1, arr2)
   result = []
   while !arr1.empty? && !arr2.empty? do
-    arr1[0] < arr2[0] ? result.push(arr1.shift) : result.push(arr2.shift) 
-  end  
-   result + arr1 + arr2
+  arr1[0] < arr2[0] ? result.push(arr1.shift) : result.push(arr2.shift)
+  end
+  result + arr1 + arr2
 end  
 
 def sort (arr)
-  if arr.length == 1 
+  if arr.length == 1
     return arr
-  end 
-  divided_array = divide_array(arr)
-  left_channel = sort(divided_array[0].slice(0, arr.length - 1))
-  right_channel = sort(divided_array[1].slice(0, arr.length - 1))
+  end
+  divided_arr = split(arr)
+  left_channel = sort(divided_arr[0])
+  right_channel = sort(divided_arr[1])
   merge(left_channel, right_channel)
-end 
+end
 
-arr = [6, 3, 1, 8, 2, 5, 4, 7]
+arr = [4, 5, 2, 3, 7, 9, 1, 8, 6]
 puts sort(arr)
+
